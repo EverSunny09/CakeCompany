1de6474fb3b44b14239679e680d3a606a97a9635
Sunny George <georgesunny1997@gmail.com>
2022-10-15 18:05:18 +0100

    Changes in Program.cs

--- CakeCompany/Models/Product.cs
+++ CakeCompany/Models/Product.cs
@@ -2,7 +2,7 @@
 
 internal class Product
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public Cake Cake { get; set; }
     public int Quantity { get; set; }
     public int OrderId { get; set; }
--- CakeCompany/Program.cs
+++ CakeCompany/Program.cs
@@ -1,10 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using CakeCompany.Provider;
-using CakeCompany.Service;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
-using Serilog.Events;
 using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("CakeCompany.UnitTest")]
@@ -13,15 +10,16 @@ using System.Runtime.CompilerServices;
 public class Program {
    
     public static void Main(string[] args) {
-        Log.Logger = new LoggerConfiguration()
+
+      Log.Logger = new LoggerConfiguration()
      .MinimumLevel.Debug()
      .WriteTo.Console()
      .CreateLogger();
 
-        
-
         Console.WriteLine("Shipment Details...");
+        Console.ReadLine();
         Log.CloseAndFlush();
+
     }
 }
 
